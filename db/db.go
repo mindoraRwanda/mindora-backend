0e350ec42c36eaea1f381365251b07d780611721
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-07-11 16:24:03 +0200

    create db life

---
+++ db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+    "context"
+    "os"
+
+    "github.com/jackc/pgx/v4"
+)
+
+func Connect() (*pgx.Conn, error) {
+    connStr := os.Getenv("DATABASE_URL")
+    if connStr == "" {
+        return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
+    }
+
+    conn, err := pgx.Connect(context.Background(), connStr)
+    if err != nil {
+        return nil, err
+    }
+
+    err = conn.Ping(context.Background())
+    if err != nil {
+        return nil, err
+    }
+
+    return conn, nil
+}
